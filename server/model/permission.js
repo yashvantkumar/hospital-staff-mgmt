9c9b0132996584cf146312fb2cf1d5c5744dfda5
Yashwant Shirol <yshirol94@gmail.com>
2023-09-21 13:14:00 +0530

    Update: Schema changes for permission collection

--- server/model/permission.js
+++ server/model/permission.js
@@ -4,9 +4,11 @@ const Schema = mongoose.Schema;
 const PermissionSchema = new Schema({
     id: { type: String, required: true, unique: true },
     name: { type: String, required: true },
-    formattedName: { type: String, required: true, enum: ["NURSE", "DOCTOR", "SECURITY", "ATTENDER", "ADMIN"] },
+    permissions: [{
+        type: String,
+        enum: ["CREATE", "READ", "UPDATE", "DELETE"]
+    }],
     description: { type: String, required: false },
-    permissionId: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
 });
